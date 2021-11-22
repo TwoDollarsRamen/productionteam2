cad6094ac11cf6e3c3b76ed67461d6600c9cb244
s191825 <s191825@students.aie.edu.au>
2021-11-22 16:41:15 +1100

    Revert "Update Jump.cs"
    This reverts commit 82cffe75cb4ef743f63b7aa1b9f3e8d1bfa548b8.

--- Night Runner/Assets/Scripts/Jump.cs
+++ Night Runner/Assets/Scripts/Jump.cs
@@ -1,12 +1,9 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float jumpHeight;
-
     // Start is called before the first frame update
     void Start()
     {
