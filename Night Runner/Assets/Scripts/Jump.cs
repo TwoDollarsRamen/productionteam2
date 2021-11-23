fa51cc98fbc441bf8c7749326e11ce46655eb9bb
s191825 <s191825@students.aie.edu.au>
2021-11-23 11:18:29 +1100

    Changed Movement
    Movement now works good with force.

--- Night Runner/Assets/Scripts/Jump.cs
+++ Night Runner/Assets/Scripts/Jump.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float jumpHeight;
+    public float jumpHeight = 10;
+    public bool isGrounded;
+
+    Rigidbody rb; // refers to red riding hood cubes rigid body
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // jump control
+        {
+            rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
+        }
+    }
+    /// <summary>
+    /// If the bottom of the player collides with an object, set isGrounded to true
+    /// </summary>
+    void OnCollisionStay(Collision collidedObject)
+    {
+        ContactPoint contact = collidedObject.contacts[0];
+        if (Vector3.Dot(contact.normal, Vector3.up) > 0.5) // checks if contact was below
+        {
+            isGrounded = true;
+        }
+    }
+    bool CheckStuck() // not implemented yet but will check if they are stuck (colliding with wall) and help the player jump out.
+    {
+        return false;
+    }
+    /*
+    void OnCollisionStay() // option if you want 'wall jumping'
+    {
+        isGrounded = true;
+    }
+    */
+    /// <summary>
+    /// Whenever the player stops colliding with the floor, allow them to jump again.
+    /// </summary>
+    void OnCollisionExit()
+    {
+        isGrounded = false;
     }
 }
--- Night Runner/Assets/Scripts/RedRidingHood.cs
+++ Night Runner/Assets/Scripts/RedRidingHood.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class RedRidingHood : MonoBehaviour
 {
-	public float moveForce = 7;
+	public float moveForce = 10;
 
 	Rigidbody rb;
 
 	void Start()
 	{
 		rb = gameObject.GetComponent<Rigidbody>();
+	}
 
-		rb.AddForce(new Vector3(moveForce, 0.0f, 0.0f));
+    private void Update()
+    {
+		//transform.Translate(maxSpeed * Time.deltaTime, 0, 0);
+
+		if (rb.velocity.x < moveForce) // limit speed
+		{
+			rb.AddForce(new Vector3(moveForce, 0.0f, 0.0f));
+		}
 	}
 }
