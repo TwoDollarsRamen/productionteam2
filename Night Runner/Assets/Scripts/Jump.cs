b36e203daa3258f71a24b7d810fc9d785bb8de02
s191825 <s191825@students.aie.edu.au>
2021-11-25 14:20:59 +1100

    Updated jump
    Made it so that jump increases with how long you hold it down, with 3 stages.

--- Night Runner/Assets/Scripts/Jump.cs
+++ Night Runner/Assets/Scripts/Jump.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float jumpHeight = 10;
+    float jumpHeight = 10;
+
+    public float minJumpHeight;
+
+    [Tooltip("How much the jump height will increase by each interval")]
+    public float jumpHeightIncrease;
+
     public bool isGrounded;
+
+    float jumpStartTime;
+    float heldTime;
+
+    [Tooltip("How often or quickly the jump height will increase")]
+    public float increaseInterval;
 
     bool jump = false;
 
@@ -16,22 +28,50 @@ public class Jump : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    // FixedUpdate is called once per frame
-    void FixedUpdate()
-    {
-        if (jump == true)
-        {
-            rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
-            jump = false;
-        }
-    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // jump control
         {
+            jumpStartTime = Time.time;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded) // jump control
+        {
+            heldTime = Time.time - jumpStartTime;
             jump = true;
         }
+    }
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        if (jump == true)
+        {
+            if (heldTime < increaseInterval)
+            {
+                jumpHeight = minJumpHeight;
+            }
+            else if (heldTime >= increaseInterval && heldTime < increaseInterval * 2)
+            {
+                jumpHeight = minJumpHeight + jumpHeightIncrease;
+            }
+            else
+            {
+                jumpHeight = minJumpHeight + jumpHeightIncrease * 2;
+            }
+            rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
+            jump = false;
+        }
+
+        /*
+        if (Input.GetKeyDown("w"))
+        {
+            startTime = Time.time;
+        }
+        if(Input.GetKeyUp("w"))
+        {
+            Debug.Log((Time.time - startTime).ToString("00:00.00"));
+        }
+         */
     }
     /// <summary>
     /// If the bottom of the player collides with an object, set isGrounded to true
