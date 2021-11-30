98da21d282f3350fd6058e972a06293b3a7b5a8f
s191825 <s191825@students.aie.edu.au>
2021-11-30 13:31:23 +1100

    Added WinloseMenu

--- Night Runner/Assets/Scripts/WinLoseMenu.cs
+++ Night Runner/Assets/Scripts/WinLoseMenu.cs
@@ -2,34 +2,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEditor;
 using UnityEngine.UI;
 
 public class WinLoseMenu : MonoBehaviour
 {
-    public Button restartButton = null;
-    public Button exitButton = null;
-    public Image background = null;
+    public Button restartButton;
+    public Button exitButton;
+    public Image background;
+
+    bool gamePaused = false;
+    bool pauseEnabled = true;
+
+    public bool gameWon = false;
+    public bool gameLost = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //restartButton.isActiveAndEnabled;
-        //exitButton.SetActive(false);
-        //background.SetActive(false);
-
-        //background = GameObject.Find("WinLoseBackground");
-        //restartButton = GameObject.Find("RestartButton");
-        //exitButton = GameObject.Find("ExitButton");
+        restartButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+        background.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) // jump control
+        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == false && pauseEnabled == true)
         {
-            Time.timeScale = 0.0f; // pause
+            GamePause();
         }
+        else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true && pauseEnabled == true)
+        {
+            GameUnPause();
+        }
+
+        exitButton.onClick.AddListener(Exit);
+    }
+    void GamePause()
+    {
+        restartButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+
+        Time.timeScale = 0.0f; // pause
+        gamePaused = true;
+    }
+    void GameUnPause()
+    {
+        restartButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+        background.gameObject.SetActive(false);
+
+        Time.timeScale = 1.0f; // unpause
+        gamePaused = false;
+    }
+
+    public void WinLoseResult(bool playerWon)
+    {
+        restartButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+
+        Time.timeScale = 0.0f; // pause
+        pauseEnabled = false;
+    }
+    void Exit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
