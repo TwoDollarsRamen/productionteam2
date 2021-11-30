08822debea6784e26f4c107aa09b33f2edbeba77
George Lamb <s211427@students.aie.edu.au>
2021-11-30 16:27:35 +1100

    Make wolf move fowards with sanity

--- Night Runner/Assets/Scripts/WolfMovement.cs
+++ Night Runner/Assets/Scripts/WolfMovement.cs
@@ -7,18 +7,33 @@ public class WolfMovement : MonoBehaviour
     public Camera mainCamera;
     CameraMover mover;
 
+    public Sanity player;
+    public float sanityCloseMultiplier = 1.1f;
+
     public bool touchingPlayer = false;
+
+	float initialCameraOffset;
+
+	Vector3 targetPosition;
+	Vector3 oldPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         mover = mainCamera.GetComponent<CameraMover>();
+
+        initialCameraOffset = (transform.position - mainCamera.transform.position).x;
+        targetPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(mover.cameraSpeed * Time.deltaTime, 0, 0);
+    	var unitSanity = (float)player.maxSanity - (float)player.sanity;
+
+        targetPosition.x = mainCamera.transform.position.x + initialCameraOffset +
+        	unitSanity * sanityCloseMultiplier;
+        transform.position = targetPosition;
     }
 
     private void OnCollisionEnter(Collision collision)
